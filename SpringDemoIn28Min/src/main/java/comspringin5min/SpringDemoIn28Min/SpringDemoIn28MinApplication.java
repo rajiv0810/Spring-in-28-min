678892c5ce50f30e95b1f9e645b2e4e11bd47c4d
Raju Singh <rajiv.icps@gmail.com>
2018-08-29 01:15:49 +0530

    Spring Demo

---
+++ SpringDemoIn28Min/src/main/java/comspringin5min/SpringDemoIn28Min/SpringDemoIn28MinApplication.java
@@ -0,0 +1,12 @@
+package comspringin5min.SpringDemoIn28Min;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoIn28MinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoIn28MinApplication.class, args);
+	}
+}
---
+++ SpringDemoIn28Min/src/test/java/comspringin5min/SpringDemoIn28Min/SpringDemoIn28MinApplicationTests.java
@@ -0,0 +1,16 @@
+package comspringin5min.SpringDemoIn28Min;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDemoIn28MinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
